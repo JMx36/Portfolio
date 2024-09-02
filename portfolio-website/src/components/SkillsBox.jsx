807da26c744079c9a21dfec92b43c688c1ba7314
Josh <jcpm2603@gmail.com>
2024-09-01 23:23:36 -0700

    edits to skillbox component

--- portfolio-website/src/components/SkillsBox.jsx
+++ portfolio-website/src/components/SkillsBox.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import {LogoImage} from "../components/Skills.jsx"
+import {LogoImage} from "../components/utilities.jsx"
 
 
-const SkillsBox = ({title, color="white", logos=[]}) => {
+const SkillsBox = ({title, color="white", logos=[], animation=""}) => {
   return (
-    <div className={"skills-box border-black"} style={{backgroundColor: color, color: "black"}}>
+    <div className={`skills-box border-black ${animation}`} style={{backgroundColor: color, color: "black", overflow: "hidden"}}>
         
-        <div className="skill-box-container border-blue" style={{fontSize: "32px"}}>
+        <div className="skill-box-container" style={{fontSize: "32px"}}>
             <h3 style={{width: "100%"}}>{`${title}:`}</h3>
-            <div style={{display: "flex", flexWrap: "wrap", gap: "10%"}}>
-                {logos.map((logo) => <LogoImage setIsHovered={undefined} 
+            <div className="border-black" style={{display: "flex", flexWrap: "wrap", gap: "5%"}}>
+                {logos.map((logo, index) => <LogoImage setIsHovered={undefined} 
                                                 image={logo.image}
                                                 index={index % 12}      
-                                                width="4%"
-                                                height="4%"
+                                                width="20%"
+                                                height="20%"
                                                 margin_left=""
                                                 />)}
             </div>

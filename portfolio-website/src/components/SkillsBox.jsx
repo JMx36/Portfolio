51d3de5b3be83ed0cb4adb0cd34fd43819cdd8f3
Josh <jcpm2603@gmail.com>
2024-09-07 01:46:06 -0700

    skills dropdown support for small screens v1

--- portfolio-website/src/components/SkillsBox.jsx
+++ portfolio-website/src/components/SkillsBox.jsx
@@ -7,7 +7,7 @@ const SkillsBox = ({title, color="white", logos=[], animation=""}) => {
     <div className={`skills-box border-black ${animation}`} style={{backgroundColor: color, color: "black", overflow: "hidden", textAlign: "left"}}>
         
         <div className="skill-box-container fs-32px">
-            <h3 style={{margin: "0% auto 5% 0%"}}>{`${title}:`}</h3>
+            <h3 style={{margin: "0% auto 5% 0%", paddingLeft: "10px", paddingRight: "10px"}}>{`${title}:`}</h3>
             <div className="images-container " style={{display: "flex", alignItems: "center", flexWrap: "wrap", gap: "5%"}}>
                 {logos.map((logo, index) => <LogoImage setIsHovered={undefined} 
                                                 image={logo.image}

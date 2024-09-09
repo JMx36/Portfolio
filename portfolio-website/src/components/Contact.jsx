d1970d334f8c2451e654072ecae71b52ce97949e
Josh <jcpm2603@gmail.com>
2024-09-08 23:31:36 -0700

    contact logos width and centering fixed

--- portfolio-website/src/components/Contact.jsx
+++ portfolio-website/src/components/Contact.jsx
@@ -1,32 +1,35 @@
 import React from 'react'
 import linked_in from "../assets/Images/Logos/linked_in.png"
 import mail from "../assets/Images/Logos/mail.png"
+import { useMediaQuery } from 'react-responsive';
 
 const Contact = () => {
 
+  const isSmallerScreen = useMediaQuery({ query: '(max-width: 1000px)' });
+
   const image_style = 
   {
-    width: '30%',
+    width: "30%"
   }  
   
   const circulay_mask =
   {
     height: "100%",
-    width: "90%",
-    
+    width: "100%",
     backgroundImage: "url('path-to-your-image.jpg')", 
     backgroundSize: "cover",
     borderRadius: "50%", /* Makes the element circular */
     backgroundColor: "#D9D9D9",
-    margin: "0% auto"
+    margin: "5% auto"
   }
 
   return (
     <div className="contact-section"> 
         <div className="contact-section-title">
-            <h1 className='text sub-title-text'>Contact Me</h1>
+            <h1 className='sub-title-text' style={{margin: "0"}}>Contact Me</h1>
         </div>  
         <div className="contact-logos-container">
+            
             <img className="" src={linked_in} alt="linkedIn" style={image_style} />
             <div className="name-section">
                 <div className="contact-photo-view">
@@ -34,7 +37,7 @@ const Contact = () => {
                 </div>
                 <h3 className="contact-name cursive-button">Josh Castillo</h3>
             </div>
-            <img className="" src={mail} alt="email" style={image_style}/>
+            <img src={mail} alt="email" style={image_style}/>
         </div>
     </div>
   )

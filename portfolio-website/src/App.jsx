91455d0112ab85e21a0f1edaf4ffe2a70f9ef8a7
Josh <jcpm2603@gmail.com>
2024-09-18 02:00:02 -0700

    added gaming portfolio page

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -5,12 +5,14 @@ import { Route, createBrowserRouter,
 
 
 import HomePage from './pages/HomePage.jsx';
+import GamingPortfolio from './pages/GamingPortfolio.jsx';
 import MainLayout from './layouts/MainLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route path="index.html" element={<HomePage />} />
+      <Route path="gaming-portfolio" element={<GamingPortfolio />} />
     </Route>
   )
 );
---
+++ portfolio-website/src/pages/GamingPortfolio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Hero from '../components/Hero.jsx';
+import CircularSelector from '../components/CircularSelector.jsx';
+import Contact from '../components/Contact.jsx';
+
+const GamingPortfolio = () => {
+  return (
+    <>
+        <Hero />
+        <CircularSelector />
+        <Contact />
+    </>
+  )
+}
+
+export default GamingPortfolio

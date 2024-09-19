8cd7e95e7f9465cbfaa0dea3f81afe9813f76716
Josh <jcpm2603@gmail.com>
2024-09-18 21:30:31 -0700

    circular selector buttons and set up for content

--- portfolio-website/src/components/CircularSelector.jsx
+++ portfolio-website/src/components/CircularSelector.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import c_logo from "../assets/Images/TempLogos/c_processed1.png"
 
-const Button = ({text}) => 
+const Button = ({text, index, isOn, setIndex}) => 
 {
 
+    const [isHovered, SetHovered] = useState(false);
 
     const circle_style =
     {
@@ -19,12 +21,44 @@ const Button = ({text}) =>
     }
 
     return(
-        <div style={{width: "100%", height: "100%", position: "relative"}}>
+        <div className="" style={{width: "100%", maxWidth: "200px", height: "100%", position: "relative", textAlign: "center"}}>
             
-            <span className="normal-text-medium" style={{position: "relative", padding: "1rem 2rem"}}>
+            <span className="normal-text-medium cursor-pointer" style={
+                {
+                display: "inline-block",
+                boxSizing: "border-box",
+                position: "relative", 
+                padding: "1rem 2rem",
+                textShadow: isOn ? "" : "0px 3.5px 4px rgba(82, 82, 82, 0.63)",
+            }} 
+                onClick={() => 
+                    setIndex(isOn ? -1 : index)
+                }
+                onMouseEnter={() => SetHovered(true)}
+                onMouseLeave={() => SetHovered(false)}
+                >
                 {text}
-                <div style={circle_style}></div>
+                {isOn || isHovered ? <div style={circle_style}></div> : ''}
             </span>
+        </div>
+    )
+}
+
+
+const BlurCircleContent = ({isExpanded, content=[1, 2, 3, 5, 6, 7, 8, 9 ,10]}) => 
+{
+    return(
+        <div style={{width: "100%", height: "100%", position: "relative"}}>
+            <div className='blur-circle'></div>
+            <div className='text'>
+                { isExpanded ? 
+                    content.map((item) => 
+                    (
+                        <img src={c_logo} alt="" />
+                    )
+                    ) : ''
+                }
+            </div>
         </div>
     )
 }
@@ -33,8 +67,15 @@ const Button = ({text}) =>
 
 const CircularSelector = () => {
    
-    const buttons_text = ["Prodcution", "Engines", "Software", "Languages", "Version Control"]
+    const buttons_text = ["Production", "Engines", "Version Control", "Software", "Languages"]
 
+    const [currentIndex, SetIndex] = useState(1);
+
+    const selector_circle_style =
+    {
+        height: "60%",
+        width: "30%"
+    }
 
     return (
         <div className="circular-skills-section">
@@ -44,16 +85,16 @@ const CircularSelector = () => {
                     I find joy in exploring and mastering various technologies. 
                     Here are some of the technologies I've had the pleasure of working with.
                 </p>
-                <div className="circular-skills-container">
-                    <div className='selector-circle'>
-                        <div style={{width: "100%", height: "100%", position: "relative"}}>
-                            <div className='blur-circle'></div>
-                            {/* <p className="text">Text</p> */}
-                        </div>
+                <div className="circular-skills-container border-black">
+                    <div className={`selector-circle ${currentIndex < 0 ? 'blur-circle-idle-anim' : ''}`} 
+                        style={selector_circle_style}>
+                        <BlurCircleContent isExpanded={currentIndex >= 0}/>
                     </div>
                     {
                         buttons_text.map((button_info, index) => (
-                            <div className={`absolute-center move-corner${index + 1}`}><Button text={button_info}/></div>
+                            <div className={`absolute-center move-corner${index + 1}`}>
+                                <Button text={button_info} index={index} isOn={index === currentIndex} setIndex={SetIndex}/>
+                            </div>
                         ))
                     }
                 </div>

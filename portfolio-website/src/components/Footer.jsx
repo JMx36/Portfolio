3405cc9cef1066740059dd183f0328a65298c492
Josh <jcpm2603@gmail.com>
2024-09-20 05:36:55 -0700

    footer changed

--- portfolio-website/src/components/Footer.jsx
+++ portfolio-website/src/components/Footer.jsx
@@ -8,14 +8,14 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className='footer-container'>
-                <h3 className='cursive-button' style={{margin: "0"}}>Josh Castillo</h3>
+                <h3 className='pacifico-family fs-36px fw-400' style={{margin: "0"}}>Josh Castillo</h3>
                 <div className="footer-logo-container">
                     <LogoImage image={linked_in} width="43px"/>
                     <LogoImage image={github} width="43px"/>
                     <LogoImage image={linked_in} width="43px"/>
                     <LogoImage image={mail} width="43px"/>
                 </div>
-                <p className='thin-white-text' style={{margin: "0"}}>© 2024. All Rights Reserved.</p>
+                <p className='work-sans-family fs-20px fw-300' style={{margin: "0"}}>© 2024. All Rights Reserved.</p>
             </div>
         </div>
     )

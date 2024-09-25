8b76d6e0e2b3ca38f6ae75934916e613aba6752c
Josh <jcpm2603@gmail.com>
2024-09-24 19:18:49 -0700

    portfolio item made responsive

--- portfolio-website/src/components/PortfolioItem.jsx
+++ portfolio-website/src/components/PortfolioItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Button} from "../components/utilities.jsx"
+import { useMediaQuery } from 'react-responsive';
+
 
 const TaskDescription = ({task_title, tasks}) =>
 {
@@ -15,7 +17,33 @@ const TaskDescription = ({task_title, tasks}) =>
     )
 }
 
-const PortfolioItem = ({youtube_vid="https://www.youtube.com/embed/OQ1CwPhE8KQ", tasks=[], images=[]}) => {
+
+const GameTitleInfo = ({title, subTitle, game_info, description}) => {
+    return (
+        <div className='text-container fs-40px work-sans-family fw-600'>
+            <h2 style={{marginBottom: "2rem"}}>{title}</h2>
+            <p className='work-sans-family fs-24px fw-600'>{subTitle}</p>
+            <p className='work-sans-family fs-20px fw-300' style={{marginTop: "2%"}}>{game_info}</p>
+            <p className='work-sans-family fs-20px fw-300' style={{marginTop: "2%", fontStyle: "italic"}}>
+                {description}
+            </p>
+            <Button text="Try it out!" 
+                                text_style={"work-sans-family fs-24px fw-600"} 
+                                radius="10px"
+                                style={{color: "white", padding: "1rem 1rem", margin: "4% auto 0% auto"}}
+
+                        />
+        </div>
+    )
+}
+
+const PortfolioItem = ({title="", subTitle="", game_info="", description="", 
+        youtube_vid="https://www.youtube.com/embed/OQ1CwPhE8KQ", tasks=[], images=[]}) => {
+
+    title="Quickshot";
+    subTitle="Gameplay Engineer | Programming Lead";
+    game_info="(Unity | PC)";
+    description="A sniper free-for-all (FFA) game, where the sniper is inspired from Overwatch's character, Ana.";
 
     tasks.push(["Destruction Strike Team Member", [
         "Was part of a core engineering effort to implement fine-grain destruction, a marquee feature for the title",
@@ -28,24 +56,10 @@ const PortfolioItem = ({youtube_vid="https://www.youtube.com/embed/OQ1CwPhE8KQ",
         <div className="portfolio-item">
             <div className='title-section dark-overlay'>
                 <div className='container to-front2'>
-                    <div className='text-container fs-40px work-sans-family fw-600'>
-                        <h2 style={{marginBottom: "10%"}}>QuickShot</h2>
-                        <p className='work-sans-family fs-24px fw-600'>Gameplay Engineer | Programming Lead</p>
-                        <p className='work-sans-family fs-20px fw-300' style={{marginTop: "2%"}}>(Unity | PC)</p>
-                        <p className='work-sans-family fs-20px fw-300' style={{marginTop: "2%", fontStyle: "italic"}}>
-                            A sniper free-for-all (FFA) game, where the sniper is inspired from Overwatch's character, Ana.
-                        </p>
-                        <Button text="Try it out!" 
-                                text_style={"work-sans-family fs-24px fw-600"} 
-                                radius="10px"
-                                style={{color: "white", padding: "1rem 1rem", margin: "4% auto 0% auto"}}
-
-                        />
-                    </div>
-                    <iframe  src={youtube_vid}
-                        width="640" height="360" title="YouTube embed" allow="autoplay; fullscreen" allowFullScreen>
-                    </iframe>
-
+                        <GameTitleInfo title={title} subTitle={subTitle} game_info={game_info} description={description}/>
+                        <iframe src={youtube_vid}
+                            title="YouTube embed" allow="autoplay; fullscreen" allowFullScreen>
+                        </iframe> 
                 </div>
             </div>
             <div className='tasks-section'>
